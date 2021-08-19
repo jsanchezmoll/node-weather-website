bb7cfb8e9449bf63eb29760fa3103236df4bd908
Juan Sánchez <jsanchez@mollitiam.local>
2021-08-19 13:20:42 +0200

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,10 @@ const forecast = (latitude, longitud, callback) => {
         } else if (body.error){
             callback('Unable to find location', undefined)
         } else{
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. Therre is a ' + body.current.precip +  '% chance of rain')
+            callback(
+                undefined, 
+                body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + 
+                body.current.feelslike + ' degrees out. The humidity is '+ body.current.humidity + '%.')
         }
     })
 }
